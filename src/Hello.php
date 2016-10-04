cd5538c10e7b07da072e4a9f614013b50565a055
Pedro Martinez <pmartinez1085@gmail.com>
2016-10-04 16:11:36 +0200

    index

---
+++ src/Hello.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace pmartinez85;
+
+class Hello
+{
+    public function hello(){
+        return 'hello World';
+    }
+}
---
+++ tests/index.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace pmartinez85\Hello;
+
+$hello = new Hello();
+
+
+//Legitibilitat
+
+echo $hello->say();
